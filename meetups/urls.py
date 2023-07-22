064792b6f30920bec44a301e428a4bd631a4ded0
Rohit Karna <rohitkarn627@gmail.com>
2023-07-23 02:37:24 +0545

    Template Inheritance Configured

--- meetups/urls.py
+++ meetups/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('meetups/', views.index, name='index'),
+    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-details'),
 ]
--- meetups/views.py
+++ meetups/views.py
@@ -19,3 +19,15 @@ def index(request):
     return render(request, 'meetups/index.html', {
         'meetups': meetups
     })
+
+
+def meetup_details(request, meetup_slug):
+    print(meetup_slug)
+    selected_meetup = {
+        'title': 'A First Meetup',
+        'description': 'This is the first Meetup'
+    }
+    return render(request, 'meetups/meetup-details.html', {
+        'meetup_title': selected_meetup['title'],
+        'meetup_description': selected_meetup['description']
+    })
